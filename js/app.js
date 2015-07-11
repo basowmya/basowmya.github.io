5297669607a6bcf36565992d61d3e12af53be310
Dheeraj <dheerajvs@gmail.com>
2015-07-11 22:54:25 +0530

    home page completed

--- js/app.js
+++ js/app.js
@@ -7,25 +7,81 @@ var React = require('react'),
   Nav = require('react-bootstrap').Nav,
   NavItem = require('react-bootstrap').NavItem,
   DropdownButton = require('react-bootstrap').DropdownButton,
-  MenuItem = require('react-bootstrap').MenuItem;
+  MenuItem = require('react-bootstrap').MenuItem,
+  Grid = require('react-bootstrap').Grid,
+  Row = require('react-bootstrap').Row,
+  Col = require('react-bootstrap').Col,
+  Thumbnail = require('react-bootstrap').Thumbnail,
+  data = require('./data');
 
-var App = React.createClass({
+var Navigation = React.createClass({
   render: function () {
-    return <Navbar brand="Sowmya's Art Gallery">
-      <Nav>
-        <NavItem eventKey={1} href='#'>Home</NavItem>
-        <DropdownButton eventKey={2} title='Gallery'>
-          <MenuItem eventKey='1'>Oil Paintings</MenuItem>
-          <MenuItem eventKey='2'>Kalamkari</MenuItem>
-          <MenuItem eventKey='3'>Zentangles</MenuItem>
-          <MenuItem eventKey='4'>Others</MenuItem>
-        </DropdownButton>
-        <NavItem eventKey={3} href='#'>About</NavItem>
-        <NavItem eventKey={4} href='#'>Contact</NavItem>
-      </Nav>
-    </Navbar>;
+    return (
+      <Navbar brand="Sowmya's Art Gallery">
+        <Nav>
+          <NavItem eventKey={1} href='#'>Home</NavItem>
+          <DropdownButton eventKey={2} title='Gallery'>
+            {
+              this.props.categories.map(function (category, index) {
+                return (
+                  <MenuItem key={category.id} eventKey={index + 1}>
+                    {category.name}
+                  </MenuItem>
+                );
+              })
+            }
+          </DropdownButton>
+          <NavItem eventKey={3} href='#'>About</NavItem>
+          <NavItem eventKey={4} href='#'>Contact</NavItem>
+        </Nav>
+      </Navbar>
+    );
   }
 });
 
-React.render(<App name="Dheeraj"></App>,
-  document.getElementById('app'));
+var Footer = React.createClass({
+  render: function () {
+    return (
+      <footer>
+        <p>Copyright &copy; 2015, Sowmya B A</p>
+      </footer>
+    );
+  }
+});
+
+var Categories = React.createClass({
+  render: function () {
+    var cols = this.props.categories.map(function (category, index) {
+      var col = [
+        <Col xs={6} sm={4}>
+          <Thumbnail href='#' alt={category.name} src={category.image} />
+        </Col>
+      ];
+
+      if ((index % 2) !== 0) {
+        col.push(<div className="clearfix visible-xs-block"></div>);
+      }
+      if (((index + 1) % 3) === 0) {
+        col.push(<div className="clearfix visible-sm-block"></div>);
+      }
+
+      return col;
+    });
+
+    return <Grid><Row>{cols}</Row></Grid>;
+  }
+});
+
+var App = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <Navigation categories={this.props.data.categories}/>
+        <Categories categories={this.props.data.categories}/>
+        <Footer/>
+      </div>
+    );
+  }
+});
+
+React.render(<App data={data}/>, document.getElementById('app'));
---
+++ js/data.js
@@ -0,0 +1,37 @@
+var data = {
+  "categories": [
+    {
+      "id": 1,
+      "name": "Oil Paintings",
+      "image": "img/items/oil-01-s.jpg"
+    }, {
+      "id": 2,
+      "name": "Kalamkari",
+      "image": "img/items/kal-01-s.jpg"
+    }, {
+      "id": 3,
+      "name": "Zentangles",
+      "image": "img/items/zen-01-s.jpg"
+    }, {
+      "id": 4,
+      "name": "Others",
+      "image": "img/items/mur-01-s.jpg"
+    }
+  ],
+  "items": [
+    {
+      "id": "kal-02",
+      "name": "Saraswathi",
+      "category": 2,
+      "price": 12000,
+      "size": "24\" x 36\" (inches) with frame",
+      "description": "Media - Acrylic on Cloth",
+      "images": {
+        "small": ["img/items/kal-02-s.jpg"],
+        "large": ["img/items/kal-02-l.jpg"]
+      }
+    }
+  ]
+};
+
+module.exports = data;
