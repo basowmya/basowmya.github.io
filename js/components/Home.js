a02cd3be515e701d69f8e23636af5b921462da41
Dheeraj <dheerajvs@gmail.com>
2015-07-18 23:53:17 +0530

    Added ‘See More’ button to home

--- js/components/Home.js
+++ js/components/Home.js
@@ -16,6 +16,7 @@ var React = require('react'),
   mui = require('material-ui'),
   Paper = mui.Paper,
   RaisedButton = mui.RaisedButton,
+  FlatButton = mui.FlatButton,
   data = require('../data');
 
 var Carousel = React.createClass({
@@ -48,7 +49,12 @@ var Categories = React.createClass({
   render: function () {
     var panels = data.categories.map(function (category, index) {
       return (
-        <Panel key={category.id} header={<RaisedButton label={category.name} primary={true} />}>
+        <Panel key={category.id} header={
+          <div>
+            <RaisedButton label={category.name} primary={true} />
+            <FlatButton className={'pull-right'} label={'See more of ' + category.name} primary={true} />
+          </div>
+        }>
           <Carousel category={category}/>
         </Panel>
       );
