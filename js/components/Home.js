7b9baa1bc908d81f42418735ee726c6ead7d561e
Dheeraj <dheerajvs@gmail.com>
2015-07-18 23:19:45 +0530

    Replaced carousel by grid in home screen

--- js/components/Home.js
+++ js/components/Home.js
@@ -10,74 +10,39 @@ var React = require('react'),
   Button = bootstrap.Button,
   Panel = bootstrap.Panel,
   Thumbnail = bootstrap.Thumbnail,
+  Grid = bootstrap.Grid,
+  Row = bootstrap.Row,
+  Col = bootstrap.Col,
   mui = require('material-ui'),
   Paper = mui.Paper,
   RaisedButton = mui.RaisedButton,
-  Slick = require('react-slick'),
   data = require('../data');
 
 var Carousel = React.createClass({
   render: function () {
-    var settings = {
-        arrows: true,
-        centerPadding: '600px',
-        dots: true,
-        draggable: true,
-        infinite: false,
-        lazyLoad: true,
-        slidesToShow: 5,
-        slidesToScroll: 4,
-        speed: 500,
-        responsive: [{
-            breakpoint: 400,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                dots: false
-            }
-        }, {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1,
-                dots: false
-            }
-        }, {
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 2,
-                dots: false
-            }
-        }, {
-            breakpoint: 992,
-            settings: {
-                slidesToShow: 4,
-                slidesToScroll: 3,
-                dots: false
-            }
-        }]
-    },
-      thumbnails = _.map(
+    var cols = _.map(
         _.filter(data.items, {category: this.props.category.id}),
-        function (item) {
+        function (item, index) {
           return (
-            <Paper zDepth={3}>
-              <Thumbnail key={item.id} href={'#'} alt={item.name} src={item.images.small[0]} />
-            </Paper>
+            <Col xs={6} sm={4} md={3} lg={2}
+            className={
+              index < 2 ? '' :
+              index < 3 ? 'hidden-xs' :
+              index < 4 ? 'hidden-xs hidden-sm' :
+              index < 6 ? 'hidden-xs hidden-sm hidden-md' :
+                          'hidden-xs hidden-sm hidden-md hidden-lg'
+            }>
+              <Paper zDepth={3}>
+                <Thumbnail key={item.id} href={'#'} alt={item.name} src={item.images.small[0]} />
+              </Paper>
+            </Col>
           );
         }
       );
 
-    return (
-      <Slick {...settings}>
-        {thumbnails}
-      </Slick>
-    );
+    return <Grid><Row>{cols}</Row></Grid>;
   }
 });
-
-// <Panel key={category.id} header={<Button>{category.name}</Button>}>
 
 var Categories = React.createClass({
   render: function () {
