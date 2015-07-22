b143cdabbfdef36df3e60a0b25639e591aaae6cc
Dheeraj <dheerajvs@gmail.com>
2015-07-22 23:17:02 +0530

    Menubar changed to position ‘Others’ gallery at the end

--- js/components/Menubar.js
+++ js/components/Menubar.js
@@ -5,41 +5,45 @@
 'use strict';
 
 var React = require('react'),
-  Navigation = require('react-router').Navigation,
+  Router = require('react-router'),
   Navbar = require('react-bootstrap').Navbar,
   Nav = require('react-bootstrap').Nav,
   NavItem = require('react-bootstrap').NavItem,
   DropdownButton = require('react-bootstrap').DropdownButton,
   MenuItem = require('react-bootstrap').MenuItem,
+  _ = require('lodash'),
   data = require('../data');
 
 var Menubar = React.createClass({
-  mixins: [Navigation],
+  mixins: [Router.Navigation, Router.State],
 
   render: function () {
     var self = this;
 
+    var galleryItems = _.chain(data.categories).rest()
+      .concat(data.categories[0]).value()
+      .map(function (category) {
+        var href = self.makeHref('gallery', category);
+        return (
+          <MenuItem key={category.id} eventKey={category.id} active={false} >
+            {category.name}
+          </MenuItem>
+        );
+      });
+
     function handleSelect(key) {
-      self.transitionTo('/gallery/' + key);
+      self.transitionTo('gallery', {id: key});
     }
 
     return (
       <Navbar brand="Sowmya's Art Gallery">
         <Nav>
-          <NavItem eventKey={1} href={'#'}>Home</NavItem>
-          <DropdownButton eventKey={2} title='Gallery' onSelect={handleSelect}>
-            {
-              data.categories.map(function (category) {
-                return (
-                  <MenuItem key={category.id} eventKey={category.id}>
-                    {category.name}
-                  </MenuItem>
-                );
-              })
-            }
+          <NavItem eventKey={1} href={'#'} active={false}>Home</NavItem>
+          <DropdownButton eventKey={2} title='Gallery' onSelect={handleSelect} active={false} >
+            {galleryItems}
           </DropdownButton>
-          <NavItem eventKey={3} href='#about'>About</NavItem>
-          <NavItem eventKey={4} href='#'>Contact</NavItem>
+          <NavItem eventKey={3} href='#about' active={false}>About</NavItem>
+          <NavItem eventKey={4} href='#contact' active={false}>Contact</NavItem>
         </Nav>
       </Navbar>
     );
