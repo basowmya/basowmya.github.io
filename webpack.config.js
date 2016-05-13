39b4a2f51382c3a036a64d46445b38da3e01eeca
Dheeraj <dheerajvs@gmail.com>
2016-05-13 23:13:44 +0530

    chore: add webpack support

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+'use strict';
+
+let path = require('path');
+let webpack = require('webpack');
+
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  entry: './js/index.js',
+  output: {
+    path: path.join(__dirname, 'js'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+       {
+         test: /\.jsx?$/,
+         exclude: /(node_modules|bower_components)/,
+         loader: 'babel-loader',
+         query: {
+          presets: [
+            'es2015',
+            'stage-0',
+            'react'
+          ],
+          cacheDirectory: 'cache'
+        }
+       }
+    ]
+  }
+  // plugins: [
+  //   new webpack.DefinePlugin({
+  //     'process.env': {
+  //       'NODE_ENV': JSON.stringify('production')
+  //     }
+  //   })
+  // ]
+};
