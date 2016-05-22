17445dcbee07b26b41ee601147761b974ba36bcc
Dheeraj <dheerajvs@gmail.com>
2016-05-22 23:51:17 +0530

    styling Gallery

--- js/components/About.js
+++ js/components/About.js
@@ -30,8 +30,7 @@ export default () => (
       }}
     />
 
-    <CardText
-    >
+    <CardText>
       <p>
         <span
           style={{
--- js/components/Gallery.js
+++ js/components/Gallery.js
@@ -4,18 +4,36 @@
 
 import React from 'react';
 import ItemGrid from './ItemGrid';
-import data from '../data';
+import {categories, items} from '../data';
 
 export default class Home extends React.Component {
 
   render() {
+    const {name, id} = categories.filter(({id}) => id === this.props.params.id)[0];
+
     return (
-      <ItemGrid
-        items={
-          data.items.filter(({category}) => category === this.props.params.id)
-        }
-        showDetails={true}
-      />
+      <div
+        style={{
+          boxShadow: '0px 2px 1px -1px #D0D0D0'
+        }}
+      >
+        <div
+          style={{
+            fontSize: '24px',
+            margin: '8px 0 24px',
+            textAlign: 'center',
+            width: '100%'
+          }}
+        >
+          {name}
+        </div>
+        <ItemGrid
+          items={
+            items.filter(({category}) => category === id)
+          }
+          showDetails={true}
+        />
+      </div>
     );
   }
 }
